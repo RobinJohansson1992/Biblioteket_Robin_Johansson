e92d1cfa3e22387c06a3330fdb861fc474b081d8
RobinJohansson1992 <robin-johansson@live.com>
2025-10-01 21:13:45 +0200

    Made a method that displays the books in the library.

--- Biblioteket_Robin_Johansson/Program.cs
+++ Biblioteket_Robin_Johansson/Program.cs
@@ -4,11 +4,11 @@
     {
         static string[][] books =
             [
-                ["1", "Harry Potter"],
-                ["2", "Sagan om ringen"],
-                ["3", "Bamse i trollskogen"],
-                ["4", "Throne of glass"],
-                ["5", "A song of ice and fire"],
+                ["1", "Harry Potter", "2"],
+                ["2", "Sagan om ringen", "3"],
+                ["3", "Bamse i trollskogen", "5"],
+                ["4", "Throne of glass", "1"],
+                ["5", "A song of ice and fire", "2"],
             ];
 
         static void Main(string[] args)
@@ -20,7 +20,7 @@
         {
             foreach (string[] book in books)
             {
-                Console.WriteLine($"{book[0]}. {book[1]}");
+                Console.WriteLine($"{book[0]}. {book[1]}: {book[2]} st.");
             }
         }
     }
